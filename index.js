bd0d8156e74a8bfe58c78e07c60ae0dafc012f23
CompSciHigh24 <csh-github@compscihigh.org>
2024-08-12 12:35:54 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const prompt = require('prompt-sync')()
+let guess=prompt("Password")
+while(guess!=="SE11"){
+  let guess2=prompt("Try again")
+}
+let competencies=["if/else","while","for","functions","objects"]
+ for(let i=1;i<=5)
